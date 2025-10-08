77fea70c1928b442f94166aa6d9e24d52f555b11
aurelienog <aurelienog@gmail.com>
2025-10-08 19:34:57 +0200

    Update and rename Valid Braces to Valid Braces.c

--- C/6kyu/Valid Braces.c
+++ C/6kyu/Valid Braces.c
@@ -1,4 +1,4 @@
-Description:
+/* Description:
 Write a function that takes a string of braces, and determines if the order of the braces is valid. It should return true if the string is valid, and false if it's invalid.
 
 This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}. Thanks to @arnedag for the idea!
@@ -17,7 +17,7 @@ Examples
 
 ---
 
-My solution :
+My solution : */
 #include <stdbool.h>
 
 bool valid_braces (const char *braces)
