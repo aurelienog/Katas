fbacfa577abbb247eae2333b6082c24903db6a50
aurelienog <aurelienog@gmail.com>
2025-10-12 16:35:15 +0200

    Update Raindrops.c

--- C/8kyu/Raindrops.c
+++ C/8kyu/Raindrops.c
@@ -33,8 +33,23 @@ void convert(char result[], int drops)
         strcat(result, "Plong");
     }
 
-    if(drops % 3 != 0 && drops % 5 && drops % 7)
+    if(drops % 3 != 0 && drops % 5 != 0 && drops % 7 != 0)
     {
         sprintf(result, "%d", drops);
     }
 }
+
+/* Better solution:
+void convert(char result[], int drops)
+{
+   if (drops % 3 == 0)
+      strcat(result, "Pling");
+   if (drops % 5 == 0)
+      strcat(result, "Plang");
+   if (drops % 7 == 0)
+      strcat(result, "Plong");
+
+   if (strlen(result) == 0)
+      sprintf(result, "%d", drops);
+}
+*/
