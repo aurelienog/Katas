4b0369e8ab08b2503069be62c42c61150d7560e9
aurelienog <aurelienog@gmail.com>
2025-10-12 16:32:29 +0200

    Create Raindrops.c

---
+++ C/8kyu/Raindrops.c
@@ -0,0 +1,40 @@
+/*Introduction
+Raindrops is a slightly more complex version of the FizzBuzz challenge, a classic interview question.
+
+Instructions
+Your task is to convert a number into its corresponding raindrop sounds.
+
+If a given number:
+
+is divisible by 3, add "Pling" to the result.
+is divisible by 5, add "Plang" to the result.
+is divisible by 7, add "Plong" to the result.
+is not divisible by 3, 5, or 7, the result should be the number as a string.
+*/
+
+#include "raindrops.h"
+#include <stdio.h>
+#include <string.h>
+
+void convert(char result[], int drops)
+{
+    if(drops % 3 == 0)
+    {
+        strcat(result, "Pling");
+    }
+
+    if(drops % 5 == 0)
+    {
+        strcat(result, "Plang");
+    }
+
+    if(drops % 7 == 0)
+    {
+        strcat(result, "Plong");
+    }
+
+    if(drops % 3 != 0 && drops % 5 && drops % 7)
+    {
+        sprintf(result, "%d", drops);
+    }
+}
