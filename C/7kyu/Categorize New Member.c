b1fc0cacf7a6c1f465d15fe4acb38351ebc9fcbd
aurelienog <aurelienog@gmail.com>
2025-10-19 18:03:08 +0200

    Create Categorize New Member.c

---
+++ C/7kyu/Categorize New Member.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+
+enum membership {OPEN = 1, SENIOR = 2};
+
+void open_or_senior (size_t n, const int members[n][2], enum membership memberships[n])
+{
+  int i = 0;
+  
+  while(i < n)
+  {
+    if(members[i][0] >= 55 && members[i][1] > 7)
+    {
+      memberships[i] = SENIOR;
+    } else {
+      memberships[i] = OPEN;
+    }
+    i++;
+  }
+  return;
+}
